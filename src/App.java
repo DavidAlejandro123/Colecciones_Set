133dc675359e776253326ffc4240d4ba93f58255
DavidAlejandro123 <alejandroariste210@gmail.com>
2026-06-23 16:52:33 -0300

    Colecciones Set (HashSet)

---
+++ src/App.java
@@ -0,0 +1,45 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Bienvenidos a la fiesta de los superheroes");
+
+        Set<String> superHeroes = new HashSet<>();
+
+        superHeroes.add("Spiderman");
+        superHeroes.add("Batman");
+        superHeroes.add("CatWoman");
+        superHeroes.add("Hulk");
+        superHeroes.add("Superman");
+        superHeroes.add("Capitan America");
+
+        //Despues llego tarde nuestro superheroe estrella
+        superHeroes.add("Iron Man");
+
+        if(superHeroes.contains("Spiderman")){
+            System.out.println("Spiderman esta en la fiesta");    
+        }
+
+        superHeroes.remove("Hulk");
+
+        if(!superHeroes.contains("Hulk")){
+            System.out.println("Hulk se ha ido de la fiesta");
+        }
+
+        //por propiedad de set, ya no agrega superman porque no permite elementos duplicados
+        superHeroes.add("Superman");
+
+        if(superHeroes.isEmpty()){
+            System.out.println("La fiesta ha terminado, ya no hay superheroes");
+        } else{
+            System.out.println("Aun quedan "+ superHeroes.size() + " superheroes");
+        }
+
+        //hashSet no guarda el orden, imprime en cualquier orden
+        System.out.println("Quienes estan aun en la fiesta?");
+        for (String superHeroe : superHeroes) {
+            System.out.println(superHeroe);
+        }
+    }
+}
